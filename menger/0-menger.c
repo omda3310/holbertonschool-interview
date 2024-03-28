1816f301dcd8dba058931000e594f32a94b55545
omda3310 <6197@holbertonstudents.com>
2024-03-28 01:18:51 +0100

    the sponge

---
+++ menger/0-menger.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "menger.h"
+
+/*
+* draw_sponge - draw one level of sponge
+* @level: level of sponge
+* @x: row index
+* @y: column index
+*/
+
+void draw_sponge(int level, int x, int y)
+{
+// Calculate the size of the current level sponge
+int size = pow(3, level);
+
+if (level == 0)
+{
+    printf("#");
+}
+else
+{
+    // Check if current position is the center of the sponge
+    if (x >= size / 3 && x < 2 * size / 3 && y >= size / 3 && y < 2 * size / 3)
+    {
+        printf(" ");
+    }
+    else
+    {
+        draw_sponge(level - 1, x % (size / 3), y % (size / 3));
+    }
+}
+}
+
+/**
+* menger - create sponge
+* @level: level of sponge
+*/
+
+void menger(int level)
+{
+if (level < 0)
+{
+return;
+}
+
+int size = pow(3, level);
+
+// Draw the Menger Sponge
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        draw_sponge(level, i, j);
+    }
+    printf("\n");
+}
+}
---
+++ menger/menger.h
@@ -0,0 +1,6 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+void menger(int level);
+void draw_sponge(int level, int x, int y);
+#endif
