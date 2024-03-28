d3af468f893f166a5bf724680824f68cb196b80b
omda3310 <6197@holbertonstudents.com>
2024-03-28 02:40:24 +0100

    fix for loop

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -20,7 +20,7 @@ void draw_sponge(int level, int x, int y)
 	if (level == 0)
 	{
 		printf("#");
-    }
+	}
 	else
 	{
 		/**
@@ -33,7 +33,7 @@ void draw_sponge(int level, int x, int y)
 		else
 		{
 			draw_sponge(level - 1, x % (size / 3), y % (size / 3));
-        }
+		}
     }
 }
 
@@ -45,19 +45,20 @@ void draw_sponge(int level, int x, int y)
 void menger(int level)
 {
 	int i, j, size1;
+
 	size1 = (int)pow(3, level);
 
     if (level < 0) return;
 
     /**
     * draw_sponge - the Menger Sponge
-    */
+	*/
     for (i = 0; i < size1; i++)
     {
 		for (j = 0; j < size1; j++)
 		{
 			draw_sponge(level, i, j);
-        }
+		}
 		printf("\n");
-    }
+	}
 }
