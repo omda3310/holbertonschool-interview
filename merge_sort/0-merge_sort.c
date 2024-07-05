c21e081dc9fe6a3dca8118fad047d489c99c2895
omda3310 <6197@holbertonstudents.com>
2024-07-06 00:17:32 +0200

    merge sort

---
+++ merge_sort/0-merge_sort.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * merge - merge an array of integers
+ *
+ * @array: The array to be printed
+ * @right: The right array to be printed
+ * @left: The left array to be printed
+ * @right_size: Number of elements in right @array
+ * @left_size: Number of elements in left @array
+ */
+
+void merge(int *array, int *left, int left_size, int *right, int right_size)
+{
+	int i, j, k;
+
+	printf("Merging...\n[left]: ");
+	print_array(left, left_size);
+	printf("[right]: ");
+	print_array(right, right_size);
+
+	i = j = k = 0;
+	while (i < left_size && j < right_size)
+	{
+		if (left[i] <= right[j])
+			array[k++] = left[i++];
+		else
+			array[k++] = right[j++];
+	}
+
+	while (i < left_size)
+		array[k++] = left[i++];
+
+	while (j < right_size)
+		array[k++] = right[j++];
+
+	printf("[Done]: ");
+	print_array(array, left_size + right_size);
+}
+
+/**
+ * merge_sort - sort an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void merge_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	size_t mid = size / 2;
+	size_t left_size = mid;
+	size_t right_size = size - mid;
+
+	int *left = (int *)malloc(left_size * sizeof(int));
+	int *right = (int *)malloc(right_size * sizeof(int));
+
+	if (left == NULL || right == NULL)
+	{
+		free(left);
+		free(right);
+		return;
+	}
+
+	for (size_t i = 0; i < left_size; i++)
+		left[i] = array[i];
+	for (size_t i = 0; i < right_size; i++)
+		right[i] = array[i + mid];
+
+	merge_sort(left, left_size);
+	merge_sort(right, right_size);
+	merge(array, left, left_size, right, right_size);
+
+	free(left);
+	free(right);
+}
