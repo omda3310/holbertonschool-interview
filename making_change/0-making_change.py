592897db1500772d288f38167f4492bb8d15189b
omda3310 <6197@holbertonstudents.com>
2024-07-13 00:36:09 +0200

    make change

---
+++ making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Make change
+"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins
+    needed to meet a given amount total
+    """
+
+    if total <= 0:
+        return 0
+
+    coin_pile = [float('inf')] * (total + 1)
+    coin_pile[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                coin_pile[i] = min(coin_pile[i], coin_pile[i - coin] + 1)
+
+    return coin_pile[total] if coin_pile[total] != float('inf') else -1
---
+++ making_change/README.md
@@ -0,0 +1,8 @@
+# Making Change
+
+This project contains a Python function that determines the fewest number
+of coins needed to meet a given amount total.
+
+## Function Prototype
+
+def makeChange(coins, total):
