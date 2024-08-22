b51f044b9c634ac2bea9a5939ded7b221e6d907a
omda3310 <6197@holbertonstudents.com>
2024-08-22 19:33:29 +0200

    all files

--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -1,8 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 int get_max(int *array, size_t size);
 void counting_sort(int *array, size_t size, int exp);
 void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 
 #endif
