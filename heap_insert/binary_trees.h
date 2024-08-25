15f6f29e9aa12fb012ad30c11bedda4a1f9b9632
omda3310 <6197@holbertonstudents.com>
2024-08-25 13:26:01 +0200

    add declaration functions

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -24,5 +24,10 @@ typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+void swap_nodes(binary_tree_t *node1, binary_tree_t *node2);
+void swapp_up(binary_tree_t *node);
+binary_tree_t *find_insertion_point(heap_t *root);
+
 
 #endif
