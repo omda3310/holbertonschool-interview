423b4a1ffa3ab5a08264ef9b3e4c9dae0d5cf16c
omda3310 <6197@holbertonstudents.com>
2024-08-27 01:05:57 +0200

    updated

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -8,19 +8,20 @@
  */
 static void print_grid(int grid[3][3])
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (j)
-				printf(" ");
-			printf("%d", grid[i][j]);
-		}
-		printf("\n");
-	}
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
 }
+
 
 /**
  * sandpiles_sum - Computes the sum of two sandpiles
