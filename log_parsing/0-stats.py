e99e7b6a2251ffd40d80d24c17fc871ecc35da17
omda3310 <6197@holbertonstudents.com>
2024-08-28 14:32:05 +0200

    log-parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-This script reads stdin line by line and computes metrics.
-"""
+"""Script that reads stdin line by line and computes metrics"""
 
 
 import sys
 
 
-# Initialize variables
 total_file_size = 0
 status_code_counts = {
-    200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
-    }
+    200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 
 def print_stats():
-    """a script that reads stdin line by line and computes metrics"""
-
     print(f"File size: {total_file_size}")
     for code in sorted(status_code_counts.keys()):
         if status_code_counts[code] > 0:
