d6da9cde7f335c78cf8c51eef77f2a0a0890fff0
omda3310 <6197@holbertonstudents.com>
2024-08-28 14:16:46 +0200

    log-parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -9,8 +9,7 @@ import signal
 
 # Initialize variables
 total_file_size = 0
-status_code_counts =
-{
+status_code_counts = {
     '200': 0, '301': 0, '400': 0, '401': 0,
     '403': 0, '404': 0, '405': 0, '500': 0
 }
