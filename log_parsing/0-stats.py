0fd6e1354e99b651b3323c049b10e60dd93c43f2
omda3310 <6197@holbertonstudents.com>
2024-08-28 15:00:24 +0200

    fix output

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,31 +1,52 @@
 #!/usr/bin/python3
 
 """
-a script that reads stdin line by line and computes metrics.
+Reads stdin line by line and computes metrics.
 """
 import sys
 
-i = 0
-FileSize = 0
-STATUS = {'200': 0, '301': 0,
-          '400': 0, '401': 0,
-          '403': 0, '404': 0,
-          '405': 0, '500': 0}
-try:
-    for line in sys.stdin:
-        i += 1
-        sp = line.split(' ')
-        if len(sp) > 2:
-            FileSize += int(sp[-1])
-            if sp[-2] in STATUS:
-                STATUS[sp[-2]] += 1
-        if i % 10 == 0:
-            print("File size: {}".format(FileSize))
-            for key, value in sorted(STATUS.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
-finally:
-    print("File size: {}".format(FileSize))
-    for key, value in sorted(STATUS.items()):
-        if value != 0:
-            print("{}: {:d}".format(key, value))
+if __name__ == "__main__":
+
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
+
+    def print_stats():
+        """
+        Prints file size
+        """
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        """
+        Checks the stdin for matches
+        """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+
+            file_size[0] += int(word[-1])
+
+            status_code = int(word[-2])
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    try:
+        for line in sys.stdin:
+            parse_stdin(line)
+
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
