4889ecc3147afe18b4806dee33e2222d7805d7d2
omda3310 <6197@holbertonstudents.com>
2024-08-31 00:11:06 +0200

    starwars api

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching data:', error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, (err, res, body) => {
+      if (err) {
+        console.error('Error fetching character data:', err);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
---
+++ starwars_api/README.md
@@ -0,0 +1,27 @@
+# Star Wars API Characters
+
+This script prints all characters of a specified Star Wars movie using the Star Wars API.
+
+## Requirements
+
+- Node.js
+- The `request` module
+
+## Installation
+
+1. **Clone the repository**:
+   ```sh
+   git clone https://github.com/omda3310/holbertonschool-interview.git
+Navigate to the project directory:
+
+cd holbertonschool-interview/starwars_api
+Install the required dependencies:
+
+npm install request
+Usage
+Make the script executable:
+
+chmod +x 0-starwars_characters.js
+Run the script with a Movie ID:
+
+./0-starwars_characters.js <Movie ID>
